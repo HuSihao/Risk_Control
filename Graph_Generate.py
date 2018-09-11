45504e1a94f9a8f61b65c565fc70de3094bc5fa2
胡思昊 <husihao@26.local>
2018-09-11 16:16:07 +0800

    modified:   Graph_Generate.py 	modified:   README.md

--- Graph_Generate.py
+++ Graph_Generate.py
@@ -11,19 +11,75 @@ attributes = ['userId','l_user_phone','order_ip','order_mac','GPS','GPS_100','GP
 g = Graph()
 g.add_vertices(data.shape[0])
 
-def main():
+# relMobile
+X = data[data['relMobile'].notnull()]
+X = X[X['relMobile']!='d41d8cd98f00b204e9800998ecf8427e']['relMobile']
+dic = {}
+for i in X.index:
+    try:
+        dic[X.loc[i]].append(i)
+    except:
+        dic[X.loc[i]] = []
+        dic[X.loc[i]].append(i)
 
-    Data = pd.read_csv('./data/graph_data.csv')
-    Data[[u'l_user_phone']].values()
+for L in dic.values(): #dic.values()是list的集合
+    if len(L)>1:#说明存在边
+        for i in xrange(len(L)):
+            for j in xrange(i+1,len(L)):
+                g.add_edges([(L[i],L[j])])
 
+# workplace
+X = data[data['workplace'].notnull()]
+X1 = X[X['workplace'] != '个体']
+X2 = X1[X1['workplace']!='自由职业']
+X3 = X2[X2['workplace']!='个体户']
+X = X3[X3['workplace']!='无']['workplace']
+dic = {}
+for i in X.index:
+    try:
+        dic[X.loc[i]].append(i)
+    except:
+        dic[X.loc[i]] = []
+        dic[X.loc[i]].append(i)
 
-    g = ig.Graph()
+for L in dic.values(): #dic.values()是list的集合
+    if len(L)>1:#说明存在边
+        for i in xrange(len(L)):
+            for j in xrange(i+1,len(L)):
+                g.add_edges([(L[i],L[j])])
 
+#'order_mac'
+X1 = data[data['order_mac'].notnull()]
+X = X1[X1['order_mac']!='未知']['order_mac']
+dic = {}
+for i in X.index:
+    try:
+        dic[X.loc[i]].append(i)
+    except:
+        dic[X.loc[i]] = []
+        dic[X.loc[i]].append(i)
 
+for L in dic.values(): #dic.values()是list的集合
+    if len(L)>1:#说明存在边
+        for i in xrange(len(L)):
+            for j in xrange(i+1,len(L)):
+                g.add_edges([(L[i],L[j])])
 
-
-if __name__ == '__main__':
-
-    main()
-
-    g = Graph()
+#全部属性建图
+for attribute in attributes:
+    print attribute,
+    X = data[data[attribute].notnull()][attribute]
+    dic = {}
+    for i in X.index:
+        try:
+            dic[X.loc[i]].append(i)
+        except:
+            dic[X.loc[i]] = []
+            dic[X.loc[i]].append(i)
+    #一个attribute的dic完成
+    for L in dic.values(): #dic.values()是list的集合
+        if len(L)>1:#说明存在边
+            for i in xrange(len(L)):
+                for j in xrange(i+1,len(L)):
+                    g.add_edges([(L[i],L[j])])
+    print 'graph edge #:',g.ecount()
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Risk_Control
+# Jimi Risk Control Model
+## 1.对用于建图的数据进行Homophilic Test
+见Homophilic Test.ipynb
+
+|Relation |Edge|CrossEdge|CrossEdgeFraction|Connectedness|Dyadicity |Heterophilicity |
+|---------|----|---------|-----------------|-------------|----------|----------------|
+|User Id  |  25135  |  0  |   0   |   |   |
+|User phone| 25135  |  0  |   0   |   |   | 
+|order ip |  9753   | 579 | 0.059 |   |   |    
+|GPS      |  18358  | 393 | 0.021 |   |   |    
+|GPS_100  |	89046  |3634 | 0.041 |   |   |    
+|GPS_1000 |  377510 |21272| 0.056 |   |   |    
+|workplace|  17594 | 860  | 0.049 |   |   |    
+|order mac|  76402 | 6024 | 0.079 |   |   |    
+|order ldeviceId | 1155 | 10 | 0.009 |    |    
+|relMobile|  2008  |  9   | 0.004 |   |   |    
+
+## 2.关系网络构建
+
+### 2.1 构建Unipartite-Graph
+见Unipartite Graph.ipynb
+### 2.2 构建Bipartite-Graph
+
+## 3.图特征提取
+### 3.1 Graph-Embedding
+### 3.2 Bipartite-Graph 统计特征抽取
+
+## 4.有监督模型分类
