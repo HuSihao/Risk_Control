b282e1d97bde1a4ddccf24ec9487df8e9a6c3079
胡思昊 <husihao@26.local>
2018-09-11 13:21:06 +0800

    new file:   DeepWalk/.idea/DeepWalk.iml 	new file:   DeepWalk/.idea/dictionaries/husihao.xml 	new file:   DeepWalk/.idea/encodings.xml 	new file:   DeepWalk/.idea/misc.xml 	new file:   DeepWalk/.idea/modules.xml 	new file:   DeepWalk/.idea/workspace.xml 	new file:   DeepWalk/deepwalk.py 	new file:   DeepWalk/example_graphs/blogcatalog.mat 	new file:   DeepWalk/example_graphs/karate.adjlist 	new file:   DeepWalk/example_graphs/p2p-Gnutella08.edgelist 	new file:   DeepWalk/example_graphs/scoring.py 	new file:   DeepWalk/graph.py 	new file:   DeepWalk/graph.pyc 	new file:   DeepWalk/randomwalk.py 	new file:   DeepWalk/skipgram.py 	new file:   DeepWalk/walks.py 	new file:   DeepWalk/walks.pyc 	new file:   DeepWalk/word2vec_basic_tf.py 	modified:   Graph_Generate.py

---
+++ DeepWalk/skipgram.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+import numpy as np
+
+
+
+
+# Step 4: Build and train a skip-gram model.
+
+
+batch_size = 128
+embedding_size = 128  # Dimension of the embedding vector.
+skip_window = 2  # How many words to consider left and right.
+num_skips = 2  # How many times to reuse an input to generate a label.
+num_sampled = 64  # Number of negative examples to sample.
+
+# We pick a random validation set to sample nearest neighbors. Here we limit the
+# validation samples to the words that have a low numeric ID, which by
+# construction are also the most frequent. These 3 variables are used only for
+# displaying model accuracy, they don't affect calculation.
+valid_size = 16  # Random set of words to evaluate similarity on.
+valid_window = 100  # Only pick dev samples in the head of the distribution.
+valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+  # Input data.
+  with tf.name_scope('inputs'):
+    train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+  # Ops and variables pinned to the CPU because of missing GPU implementation
+  with tf.device('/cpu:0'):
+    # Look up embeddings for inputs.
+    with tf.name_scope('embeddings'):
+      embeddings = tf.Variable(
+          tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+      embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+    # Construct the variables for the NCE loss
+    with tf.name_scope('weights'):
+      nce_weights = tf.Variable(
+          tf.truncated_normal(
+              [vocabulary_size, embedding_size],
+              stddev=1.0 / math.sqrt(embedding_size)))
+    with tf.name_scope('biases'):
+      nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+  # Compute the average NCE loss for the batch.
+  # tf.nce_loss automatically draws a new sample of the negative labels each
+  # time we evaluate the loss.
+  # Explanation of the meaning of NCE loss:
+  #   http://mccormickml.com/2016/04/19/word2vec-tutorial-the-skip-gram-model/
+  with tf.name_scope('loss'):
+    loss = tf.reduce_mean(
+        tf.nn.nce_loss(
+            weights=nce_weights,
+            biases=nce_biases,
+            labels=train_labels,
+            inputs=embed,
+            num_sampled=num_sampled,
+            num_classes=vocabulary_size))
+
+  # Add the loss value as a scalar to summary.
+  tf.summary.scalar('loss', loss)
+
+  # Construct the SGD optimizer using a learning rate of 1.0.
+  with tf.name_scope('optimizer'):
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+
+  # Compute the cosine similarity between minibatch examples and all embeddings.
+  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keepdims=True))
+  normalized_embeddings = embeddings / norm
+  valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings,
+                                            valid_dataset)
+  similarity = tf.matmul(
+      valid_embeddings, normalized_embeddings, transpose_b=True)
+
+  # Merge all summaries.
+  merged = tf.summary.merge_all()
+
+  # Add variable initializer.
+  init = tf.global_variables_initializer()
+
+  # Create a saver.
+  saver = tf.train.Saver()
+
+# Step 5: Begin training.
+num_steps = 100001
+
+with tf.Session(graph=graph) as session:
+  # Open a writer to write summaries.
+
+
+  # We must initialize all variables before we use them.
+  init.run()
+  print('Initialized')
+
+  average_loss = 0
+  for step in xrange(num_steps):
+    batch_inputs, batch_labels = generate_batch(batch_size, num_skips,
+                                                skip_window)
+    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+
+    # Define metadata variable.
+    run_metadata = tf.RunMetadata()
+
+    # We perform one update step by evaluating the optimizer op (including it
+    # in the list of returned values for session.run()
+    # Also, evaluate the merged op to get all summaries from the returned "summary" variable.
+    # Feed metadata variable to session for visualizing the graph in TensorBoard.
+    _, summary, loss_val = session.run(
+        [optimizer, merged, loss],
+        feed_dict=feed_dict,
+        run_metadata=run_metadata)
+    average_loss += loss_val
+
+    # Add returned summaries to writer in each step.
+    writer.add_summary(summary, step)
+    # Add metadata to visualize the graph for the last run.
+    if step == (num_steps - 1):
+      writer.add_run_metadata(run_metadata, 'step%d' % step)
+
+    if step % 2000 == 0:
+      if step > 0:
+        average_loss /= 2000
+      # The average loss is an estimate of the loss over the last 2000 batches.
+      print('Average loss at step ', step, ': ', average_loss)
+      average_loss = 0
+
+  # Save the model for checkpoints.
+
--- Graph_Generate.py
+++ Graph_Generate.py
@@ -1,8 +1,15 @@
 #-*- coding:utf-8 -*-
-import igraph as ig
 import numpy as np
+from igraph import *
 import pandas as pd
+import time
+data = pd.read_csv('../data/jimi/jimi_data/graph_data.csv')
+attributes = ['userId','l_user_phone','order_ip','order_mac','GPS','GPS_100','GPS_1000','order_ldeviceId'] #可以直接使用的attribute
+# workplace,relMobile需要经过处理 有频率很大的数据，会生成很多无用边
 
+# Initial Graph
+g = Graph()
+g.add_vertices(data.shape[0])
 
 def main():
 
